a6bfa5f07ff603b16508374598fff518e762750c
dyanechi <dyanechi@proton.me>
2023-04-14 17:38:13 +0000

    git modules

---
+++ crates/builder-mod/src/lib.rs
@@ -0,0 +1,11 @@
+#![feature(decl_macro)]
+use quote::*;
+use proc_macro::TokenStream;
+use syn::{parse_macro_input, DeriveInput, Type, parse_quote};
+
+mod with_builder;
+
+#[proc_macro_derive(WithBuilder)]
+pub fn struct_with_builder(item: TokenStream) -> TokenStream {
+    with_builder::with_builder(item)
+}
